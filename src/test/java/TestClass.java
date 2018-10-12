9fa73e606a56030f7afcf6fd1f250f0d3f72afaf
yushaopeng <ysphello0@gmail.com>
2018-10-12 19:05:57 +0800

    update dom depency to make maven install works

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,14 +1,15 @@
-import com.ysp.dao.ClientDao;
-import com.ysp.service.DemoService;
+
 
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.Test;
 @RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试
  @ContextConfiguration(locations={"classpath:spring.xml"})
-
 public class TestClass {
-
+ @Test
+  public void test(){
+     System.out.println("test");
+  }
 }
